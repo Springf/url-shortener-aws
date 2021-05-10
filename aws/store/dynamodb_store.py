697145e582550ad8757ae82cd90ac7e1801ed9cf
Feng Chun <springfeng@gmail.com>
2021-05-11 00:29:21 +0800

    separate core

---
+++ aws/store/dynamodb_store.py
@@ -0,0 +1,61 @@
+from ...core.store import Store
+import boto3
+from botocore.exceptions import ClientError
+
+class DynamoDBStroe(Store):
+    def __init__(self, table) -> None:
+        super().__init__()
+        self.__store = boto3.client('dynamodb')
+        self.__url_key = 'url'
+        self.__user_key = 'user'
+        self.__table = table
+        self.__partition_key = 'shortened_url'
+    
+    def add(self, short_url:str, url:str, user:str = None) -> bool:
+        if not short_url or not url:
+            raise ValueError('URL cannot be empty.')
+        if user is None:
+            user = ''
+        item={
+            self.__partition_key:{'S': short_url},
+            self.__url_key:{'S': url},
+            self.__user_key:{'S': user}
+        }
+        try:
+            self.__store.put_item(TableName = self.__table, Item=item, ConditionExpression=f'attribute_not_exists({self.__partition_key})')
+            return True
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'ConditionalCheckFailedException':
+                print(e.response['Error']['Message'])
+                existing_url = self.__store.get_item(TableName = self.__table, AttributesToGet = [self.__url_key], Key= { self.__partition_key : { 'S': short_url}})
+                return url == existing_url['Item'][self.__url_key]['S']
+            else:
+                raise
+
+    def get(self, short_url:str) -> str:
+        if not short_url:
+            raise ValueError('URL cannot be empty.')
+        item = self.__store.get_item(TableName = self.__table, AttributesToGet = [self.__url_key], Key= { self.__partition_key : { 'S': short_url}})
+        if 'Item' in item:
+            return item['Item'][self.__url_key]['S']
+        return None
+    
+    def update(self, short_url:str, url:str, user:str) -> bool:
+        if not short_url or not url or not user:
+            raise ValueError('URL or user cannot be empty.')
+        item = self.__store.get_item(TableName = self.__table, AttributesToGet = [self.__url_key, self.__user_key], Key= { self.__partition_key : { 'S': short_url}})
+        if 'Item' in item:
+            print(item)
+            print('abc')
+            if user != item['Item'][self.__user_key]['S']:
+                return False
+            else:
+                item={
+                    self.__partition_key:{'S': short_url},
+                    self.__url_key:{'S': url},
+                    self.__user_key:{'S': user}
+                }
+                self.__store.put_item(TableName = self.__table, Item=item)
+                return True
+        else:
+            return False
---
+++ aws/tests/test_dynamodb_store.py
@@ -0,0 +1,75 @@
+from ..store.dynamodb_store import DynamoDBStroe
+import pytest
+import boto3
+dynamo = boto3.resource('dynamodb')
+table = 'url_test_store'
+
+def clean(tableName):
+    table = dynamo.Table(tableName)
+    
+    #get the table keys
+    tableKeyNames = [key.get("AttributeName") for key in table.key_schema]
+
+    #Only retrieve the keys for each item in the table (minimize data transfer)
+    projectionExpression = ", ".join('#' + key for key in tableKeyNames)
+    expressionAttrNames = {'#'+key: key for key in tableKeyNames}
+    
+    counter = 0
+    page = table.scan(ProjectionExpression=projectionExpression, ExpressionAttributeNames=expressionAttrNames)
+    with table.batch_writer() as batch:
+        while page["Count"] > 0:
+            counter += page["Count"]
+            # Delete items in batches
+            for itemKeys in page["Items"]:
+                batch.delete_item(Key=itemKeys)
+            # Fetch the next page
+            if 'LastEvaluatedKey' in page:
+                page = table.scan(
+                    ProjectionExpression=projectionExpression, ExpressionAttributeNames=expressionAttrNames,
+                    ExclusiveStartKey=page['LastEvaluatedKey'])
+            else:
+                break
+    print(f"Deleted {counter}") 
+
+def test_add():
+    store = DynamoDBStroe(table)
+    clean(table)
+    assert store.add('short', 'long')
+    assert store.add('short1', 'long1')
+    assert store.add('short1', 'long1')
+    assert store.add('short1', 'long2') == False
+    with pytest.raises(ValueError):
+        store.add('', '')
+    with pytest.raises(ValueError):
+        store.add(None, '')
+
+def test_get():
+    store = DynamoDBStroe(table)
+    clean(table)
+    store.add('short', 'long')
+    store.add('short1', 'long1')
+    store.add('short1', 'longlong')
+    store.add('short2', 'long2')
+
+    assert store.get('short') == 'long'
+    assert store.get('short1') == 'long1'
+    assert store.get('short2') == 'long2'
+    assert store.get('short3') is None
+    with pytest.raises(ValueError):
+        store.get('')
+    with pytest.raises(ValueError):
+        store.get(None)
+
+def test_update():
+    store = DynamoDBStroe(table)
+    clean(table)
+    assert store.add('short', 'long', 'user')
+    assert store.add('short1', 'long1', 'user')
+    assert store.add('short1', 'long1', 'user')
+    assert store.get('short1') == 'long1'
+    assert store.update('short1', 'long2', 'user')
+    assert store.get('short1') == 'long2'
+    assert store.add('short1', 'longlong') == False
+    assert store.update('short', 'longlong', 'user2') == False
+    with pytest.raises(ValueError):
+        assert store.update('short1', '', '')
